fc87bad985793d4a5e4f0903ddd9be332da5e073
Restyled.io <commits@restyled.io>
2024-09-10 04:42:51 +0000

    Restyled by prettier

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,39 +1,40 @@
-(function() {
-const THEME = Object.freeze({
-  LIGHT : {
-    KEY : "LIGHT",
-    COLOR : "#F0F0F0",
-  },
-  DARK : {
-    KEY : "DARK",
-    COLOR : "#1F1F1F",
-  },
-});
+(function () {
+  const THEME = Object.freeze({
+    LIGHT: {
+      KEY: "LIGHT",
+      COLOR: "#F0F0F0",
+    },
+    DARK: {
+      KEY: "DARK",
+      COLOR: "#1F1F1F",
+    },
+  });
 
-const body = document.body;
-const button = document.getElementById("theme-toggler");
+  const body = document.body;
+  const button = document.getElementById("theme-toggler");
 
-const getCurrentTheme =
-    () => { return localStorage.getItem("wb-theme") ?? THEME.LIGHT.KEY; };
+  const getCurrentTheme = () => {
+    return localStorage.getItem("wb-theme") ?? THEME.LIGHT.KEY;
+  };
 
-const setCurrentTheme = () => {
-  const isDefaultTheme = getCurrentTheme() === THEME.LIGHT.KEY;
-  body.style.backgroundColor = THEME[isDefaultTheme ? "LIGHT" : "DARK"].COLOR;
-};
+  const setCurrentTheme = () => {
+    const isDefaultTheme = getCurrentTheme() === THEME.LIGHT.KEY;
+    body.style.backgroundColor = THEME[isDefaultTheme ? "LIGHT" : "DARK"].COLOR;
+  };
 
-const toggleTheme = () => {
-  const isDefaultTheme = getCurrentTheme() === THEME.LIGHT.KEY;
-  const newTheme = isDefaultTheme ? THEME.DARK : THEME.LIGHT;
-  localStorage.setItem("wb-theme", newTheme.KEY);
-  body.style.backgroundColor = newTheme.COLOR;
-};
+  const toggleTheme = () => {
+    const isDefaultTheme = getCurrentTheme() === THEME.LIGHT.KEY;
+    const newTheme = isDefaultTheme ? THEME.DARK : THEME.LIGHT;
+    localStorage.setItem("wb-theme", newTheme.KEY);
+    body.style.backgroundColor = newTheme.COLOR;
+  };
 
-setCurrentTheme();
-button.addEventListener("click", toggleTheme);
+  setCurrentTheme();
+  button.addEventListener("click", toggleTheme);
 
-window.addEventListener("storage", e => {
-  if (e.key === 'wb-theme') {
-    setCurrentTheme();
-  }
-});
+  window.addEventListener("storage", (e) => {
+    if (e.key === "wb-theme") {
+      setCurrentTheme();
+    }
+  });
 })();
--- Day-3/index.js
+++ Day-3/index.js
@@ -1,53 +1,59 @@
-(function() {
-const inputs = [...document.querySelectorAll('input') ];
-const textbox = document.querySelector('textarea');
-const button = document.querySelector('button');
+(function () {
+  const inputs = [...document.querySelectorAll("input")];
+  const textbox = document.querySelector("textarea");
+  const button = document.querySelector("button");
 
-const SS_FORM_KEY = 'WB_SS_FORM';
+  const SS_FORM_KEY = "WB_SS_FORM";
 
-const getForm =
-    () => { return sessionStorage.getItem(SS_FORM_KEY) ?? JSON.stringify({}); };
+  const getForm = () => {
+    return sessionStorage.getItem(SS_FORM_KEY) ?? JSON.stringify({});
+  };
 
-const populateForm = (shouldReset = false) => {
-  const form = JSON.parse(getForm());
-  Object.keys(form).forEach((key) => {
-    switch (key) {
-    case 'comments':
-      textbox.value = !shouldReset ? form[key] : '';
-      break;
-    case 'name':
-    case 'phone':
-    case 'email':
-      const input = inputs.find((i) => i.name === key);
-      input.value = !shouldReset ? form[key] : '';
-      break;
-    default:
-      break;
-    }
-  });
-};
+  const populateForm = (shouldReset = false) => {
+    const form = JSON.parse(getForm());
+    Object.keys(form).forEach((key) => {
+      switch (key) {
+        case "comments":
+          textbox.value = !shouldReset ? form[key] : "";
+          break;
+        case "name":
+        case "phone":
+        case "email":
+          const input = inputs.find((i) => i.name === key);
+          input.value = !shouldReset ? form[key] : "";
+          break;
+        default:
+          break;
+      }
+    });
+  };
 
-const clearForm = (e) => {
-  e.preventDefault();
-  populateForm(true);
-  sessionStorage.removeItem(SS_FORM_KEY);
-};
+  const clearForm = (e) => {
+    e.preventDefault();
+    populateForm(true);
+    sessionStorage.removeItem(SS_FORM_KEY);
+  };
 
-const handleFormInputUpdate = (e) => {
-  const key = e.target.name;
-  const value = e.target.value;
-  const form = JSON.parse(getForm());
-  sessionStorage.setItem(SS_FORM_KEY, JSON.stringify({
-    ...form,
-    [key] : value,
-  }));
-};
+  const handleFormInputUpdate = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    const form = JSON.parse(getForm());
+    sessionStorage.setItem(
+      SS_FORM_KEY,
+      JSON.stringify({
+        ...form,
+        [key]: value,
+      }),
+    );
+  };
 
-populateForm();
-inputs.forEach(input =>
-                   input.addEventListener('keydown', handleFormInputUpdate));
-textbox.addEventListener('keydown', handleFormInputUpdate);
-inputs.forEach(input =>
-                   input.addEventListener('change', handleFormInputUpdate));
-button.addEventListener('click', clearForm);
-}());
+  populateForm();
+  inputs.forEach((input) =>
+    input.addEventListener("keydown", handleFormInputUpdate),
+  );
+  textbox.addEventListener("keydown", handleFormInputUpdate);
+  inputs.forEach((input) =>
+    input.addEventListener("change", handleFormInputUpdate),
+  );
+  button.addEventListener("click", clearForm);
+})();
